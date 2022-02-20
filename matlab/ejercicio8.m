b07574a640f8c7b9d9474861e79dae3708133ec5
Bryan Valerio Reyes <fsocietybvr@gmail.com>
2022-02-20 13:01:58 -0600

    add ejercicio8

---
+++ matlab/ejercicio8.m
@@ -0,0 +1,34 @@
+clear, clc 
+A=[1 2 3 4;
+   5 2 7 8;
+   9 0 3 2;
+   3 4 5 4];
+   
+orden = 4;
+
+for i=1:orden;
+   A(i,i)
+endfor 
+
+fprintf('Figura de la matriz: \n')
+
+for ren =1:orden;
+  for col =1:orden;
+      fprintf('%d ',A(ren, col))
+  endfor
+  fprintf('\n')
+endfor
+
+fprintf('Transformando Matriz  \n')
+
+for ren =1:orden;
+  for col =1:orden;
+      if (col < ren ) || (col >ren)
+       A(ren, col) = 0;
+      end
+      
+      fprintf('%d ',A(ren, col))
+  endfor
+  fprintf('\n')
+endfor
+
